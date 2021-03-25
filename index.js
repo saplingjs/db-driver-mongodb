d01c0a11e74e7ed69c9ea80bb7e9566265cf09db
groenroos <oskari@groenroos.fi>
2021-03-25 00:42:10 +0000

    Automatic fixes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 
 
 /* Dependencies */
-const { MongoClient, ObjectID } = require("mongodb");
-const Interface = require("@sapling/sapling/drivers/db/Interface");
-const { console } = require("@sapling/sapling/lib/Cluster");
+const { MongoClient, ObjectID } = require('mongodb');
+const Interface = require('@sapling/sapling/drivers/db/Interface');
+const { console } = require('@sapling/sapling/lib/Cluster');
 
 /* Default values */
-const HOST = "localhost";
+const HOST = 'localhost';
 const PORT = 27017;
 
 /* Default options for each type of operation */
@@ -22,7 +22,6 @@ const mongo_options = {
 };
 
 module.exports = class Mongo extends Interface {
-
 	/**
 	 * The MongoClient instance
 	 */
@@ -42,26 +41,26 @@ module.exports = class Mongo extends Interface {
 	/**
 	 * Convert all "_id" fields with a string representation of an object ID
 	 * to the appropriate MongoDB object ID object
-	 * 
+	 *
 	 * @param {object} conditions Search query object
 	 */
 	convertObjectId(conditions) {
 		if (conditions._id) {
 			try {
 				conditions._id = new ObjectID(conditions._id);
-			} catch (er) {}
+			} catch {}
 		}
-	
+
 		return conditions;
 	}
 
 
 	/**
 	 * Establish a connection to the database server
-	 * 
+	 *
 	 * @param {object} config {name: Name of the database, host: Host IP, port: Port number}
 	 */
-	async connect({name, host, port}) {
+	async connect({ name, host, port }) {
 		/* Setup the Mongo connection */
 		this.client = new MongoClient(`mongodb://${host || HOST}:${port || PORT}?useUnifiedTopology=true`);
 
@@ -92,13 +91,12 @@ module.exports = class Mongo extends Interface {
 
 	/**
 	 * Create a collection in the database where one doesn't yet exist
-	 * 
+	 *
 	 * @param {string} name Name for the collection being created
 	 * @param {array} fields Model object
 	 */
 	async createCollection(name, fields) {
-
-		console.log("CREATE COLLECTION", name, fields);
+		console.log('CREATE COLLECTION', name, fields);
 		await this.open();
 
 		const self = this;
@@ -113,13 +111,12 @@ module.exports = class Mongo extends Interface {
 
 	/**
 	 * Create an index for the specified fields
-	 * 
+	 *
 	 * @param {string} name Name of the target collection
 	 * @param {object} fields Object of indices to create.  Key is field name, value is index type, e.g. 'unique'
 	 */
 	async createIndex(name, fields) {
-
-		console.log("CREATE INDEX", name, fields);
+		console.log('CREATE INDEX', name, fields);
 		await this.open();
 
 		/* Select the given collection */
@@ -144,14 +141,13 @@ module.exports = class Mongo extends Interface {
 
 	/**
 	 * Find one or more records for the given conditions in the given collection
-	 * 
+	 *
 	 * @param {string} name Name of the target collection
 	 * @param {object} conditions The search query
 	 * @param {object} options Driver specific options for the operation
 	 */
 	async read(name, conditions, options, references) {
-
-		console.log("READ", name, conditions);
+		console.log('READ', name, conditions);
 		await this.open();
 
 		/* If there is an _id field in constraints, create a proper object ID object */
@@ -163,14 +159,14 @@ module.exports = class Mongo extends Interface {
 		/* Plain aggregation stack */
 		const stack = [
 			{
-				'$match': conditions
+				$match: conditions
 			}
 		];
 
 		/* Handle reference fields if we have any */
-		for(const reference in references) {
+		for (const reference in references) {
 			stack.push({
-				'$lookup': references[reference]
+				$lookup: references[reference]
 			});
 		}
 
@@ -185,24 +181,24 @@ module.exports = class Mongo extends Interface {
 
 	/**
 	 * Create one new records in the given collection
-	 * 
+	 *
 	 * @param {string} name Name of the target collection
 	 * @param {object} data Data for the collection
 	 */
 	async write(name, conditions, data) {
-
-		console.log("WRITE", name, conditions, data);
+		console.log('WRITE', name, conditions, data);
 		await this.open();
 
 		/* For any reference constraints, create a proper object ID object */
-		for (const i in conditions['references']) {
-			const reference = conditions['references'][i];
-			if(data[reference])
+		for (const i in conditions.references) {
+			const reference = conditions.references[i];
+			if (data[reference]) {
 				data[reference] = new mongo.ObjectID(data[reference]);
+			}
 		}
 
 		/* Remove the raw references */
-		delete conditions['references'];
+		delete conditions.references;
 
 		/* Select the given collection */
 		const collection = await this.database.collection(name, mongo_options.collection);
@@ -218,34 +214,34 @@ module.exports = class Mongo extends Interface {
 
 	/**
 	 * Modify the given values in data in any and all records matching the given conditions
-	 * 
+	 *
 	 * @param {string} collection Name of the target collection
 	 * @param {object} conditions The search query
 	 * @param {object} data New data for the matching record(s). Omitted values does not imply deletion.
 	 */
 	async modify(name, conditions, data) {
-
-		console.log("MODIFY", name, conditions, data);
+		console.log('MODIFY', name, conditions, data);
 		await this.open();
 
 		/* If there is an _id field in constraints, create a proper object ID object */
 		conditions = this.convertObjectId(conditions);
 
 		/* For any reference constraints, create a proper object ID object */
-		for (const i in conditions['references']) {
-			const reference = conditions['references'][i];
-			if(data[reference])
+		for (const i in conditions.references) {
+			const reference = conditions.references[i];
+			if (data[reference]) {
 				data[reference] = new mongo.ObjectID(data[reference]);
+			}
 		}
 
 		/* Remove the raw references */
-		delete conditions['references'];
+		delete conditions.references;
 
 		/* Select the given collection */
-		const collection = await this.database.collection(name, mongo_options.collection);	
+		const collection = await this.database.collection(name, mongo_options.collection);
 
 		/* Update the given record with new data */
-		const result = await collection.update(conditions, {"$set": data}, mongo_options.update);
+		const result = await collection.update(conditions, { $set: data }, mongo_options.update);
 
 		await this.close();
 
@@ -255,13 +251,12 @@ module.exports = class Mongo extends Interface {
 
 	/**
 	 * Delete any and all matching records for the given conditions
-	 * 
+	 *
 	 * @param {string} collection Name of the target collection
 	 * @param {object} conditions The search query
 	 */
 	async remove(name, conditions) {
-
-		console.log("REMOVE", name, conditions);
+		console.log('REMOVE', name, conditions);
 		await this.open();
 
 		/* If there is an _id field in constraints, create a proper object ID object */
